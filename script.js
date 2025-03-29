1a3bfc32cda7dba7ed386f1e2f26d1cbcc285e57
lelido <17890168+lelido@users.noreply.github.com>
2025-03-29 15:16:02 +0900

    Add 16x16 grid

---
+++ script.js
@@ -0,0 +1,11 @@
+createGrid();
+
+function createGrid() {
+    const grid = document.querySelector("#grid");
+
+    for (let i = 0; i < 16 * 16; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
+}
