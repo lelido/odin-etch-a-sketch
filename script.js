23a7de5acc3cb5bd55ef4000af1f4dcf8eb2a398
lelido <17890168+lelido@users.noreply.github.com>
2025-03-29 15:45:03 +0900

    Change grid cell color when mouse is over it

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ createGrid();
 
 function createGrid() {
     const grid = document.querySelector("#grid");
+    grid.addEventListener("mouseover", (event) => {
+        event.target.style.cssText = "background-color: black; border-color: white;";
+    });
 
     for (let i = 0; i < 16 * 16; i++) {
         const cell = document.createElement("div");
