242f52d49ff7c8581b7ef35bd211525901ec88bf
lelido <17890168+lelido@users.noreply.github.com>
2025-03-29 20:27:58 +0900

    Add darken drawing mode
    Since darkening works by adjusting opacity, the cell border had to be
    removed.

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ grid.addEventListener("mouseover", (event) => {
     switch (activeMode) {
         case "Black":
             event.target.style.backgroundColor = "black";
+            event.target.style.opacity = 1;
             break;
         case "Random":
             event.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+            event.target.style.opacity = 1;
+            break;
+        case "Darken":
+            const backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+            if (backgroundColor === "rgb(0, 0, 0)" || backgroundColor === "rgb(255, 255, 255)") {
+                const opacity = parseFloat(window.getComputedStyle(event.target).opacity);
+                event.target.style.opacity = opacity + 0.1;
+                event.target.style.backgroundColor = "black";
+            }
             break;
     }
-
-    event.target.style.borderColor = "white";
 });
 
 const newGridButton = document.querySelector("#new-grid");
